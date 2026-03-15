25007c49642ed19385bf364bc2a42eadd72e6c1a
nexocuba <nexocuba2026@gmail.com>
2026-03-14 23:42:05 -0400

    Actualizar script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,27 @@ const menu = document.getElementById('menu');
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+function toggleMenu() {
+    document.getElementById("menuLateral").classList.toggle("activo");
+}
+
+function cerrarMenu() {
+    document.getElementById("menuLateral").classList.remove("activo");
+}
+
+/* cerrar al presionar un enlace */
+document.querySelectorAll("#menuLateral a").forEach(link => {
+    link.addEventListener("click", cerrarMenu);
+});
+
+/* cerrar al hacer click fuera */
+document.addEventListener("click", function(event) {
+
+    const menu = document.getElementById("menuLateral");
+    const boton = document.querySelector(".menu-btn");
+
+    if (!menu.contains(event.target) && !boton.contains(event.target)) {
+        menu.classList.remove("activo");
+    }
+
+});
