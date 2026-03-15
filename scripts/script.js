bf3220c09cf02073e36a23b0c7c2ad6cff9b8c66
nexocuba <nexocuba2026@gmail.com>
2026-03-14 21:13:05 -0400

    Actualizar script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,5 @@ const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
 
 btnMenu.addEventListener('click', () => {
-  if(menu.style.display === "block"){
-    menu.style.display = "none";
-  } else{
-    menu.style.display = "block";
-  }
+  menu.classList.toggle('show');
 });
